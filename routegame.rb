6ea101e8af6b053b69fd32b9a7b682fdbc2e091e
Henry <hc49436@students.westport.k12.ct.us>
2015-01-29 12:20:29 -0500

    Extra comments deleted

--- routegame.rb
+++ routegame.rb
@@ -12,20 +12,3 @@ get "/word/*" do
 end
 
 
-# get "/word/:a/:b" do
-#   word[1] = params[:b]
-#   $secondletter = word[1]
-#   "Ok, now put the third letter in the route after the second. #{word[0]}"
-# end
-#
-# get "/word/" + $firstletter.to_s + "/" + $secondletter.to_s + "/:c" do
-#   word[2] = params[:c]
-#   $thirdletter = word[2]
-#   "Ok, now put the fourth letter in the route after the third."
-# end
-#
-# get "/word/" + $firstletter.to_s + "/" + $secondletter.to_s + "/" + $thirdletter.to_s + "/:d" do
-#   word[3] = params[:d]
-#   $fourthletter = word[3]
-#   "Ok, the word is #{word[0]}#{word[1]}#{word[2]}#{word[3]}."
-# end

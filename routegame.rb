23b9b341a1a6e15d807915b0053d4d201a82c14a
Henry <hc49436@students.westport.k12.ct.us>
2015-01-31 16:53:04 -0500

    Extra comments deleted

--- routegame.rb
+++ routegame.rb
@@ -9,6 +9,8 @@ word = []
 
 get "/word/*" do
   puts params[:splat][0].split('/').inspect
+  if params[:splat][0].split('/').length < 4 then puts "Ok, put the next letter in the route after the last." end
+  if params[:splat][0].split('/').length == 4 then "Your word is " + params[:splat][0].split('/')[0].to_s + params[:splat][0].split('/')[1].to_s + params[:splat][0].split('/')[2].to_s + params[:splat][0].split('/')[3].to_s + "." end
 end
 
 
